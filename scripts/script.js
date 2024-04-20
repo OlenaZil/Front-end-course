20ffd1d41479eb20fa57a0f2b0ef2af53f599cd8
OlenaZil <zilinska35@gmail.com>
2024-04-20 19:14:46 +0300

    initial commit

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// const sort = document.getElementById('sort');
+
+// function toggleMobileMenu() {
+//     navigator.classList.toggle('open');
+// }
+
+
+
+hapiness__btn2.addEventListener("click", function(){ alert("Ви в каталозі"); });
+
+hapiness__btn1.onclick = function() {
+    alert( 'Ви в кошику' );
+ };
